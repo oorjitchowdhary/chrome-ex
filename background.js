80f7f45372ad9740aa468d317704c3c1868913a3
Oorjit Chowdhary <oorjit.chowdhary@hotmail.com>
2019-11-20 03:16:52 +0530

    first

---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: "developer.chrome.com" }
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
+  });
+
+  chrome.browserAction.onClicked.addListener(function(activeTab) {
+    var newURL = "index.html";
+    chrome.tabs.create({ url: newURL });
+  });
+});
---
+++ index.js
@@ -0,0 +1,18 @@
+    function convertCode(){
+        let pythonText = document.getElementById('python-code').value;
+
+            let rep = pythonText.replace(/if/g, "agar");
+            let rep1 = rep.replace(/==/g, "barabar");
+            let rep2 = rep1.replace(/print/g, "bhaiyo aur behno");
+            let rep3 = rep2.replace(/else:/g, "nahi toh\n");
+            let rep4 = rep3.replace(/and/g, "&&");
+            let rep5 = rep4.replace(/or/g, "||");
+            let rep6 = rep5.replace(/%/g, " mod ");
+            let rep7 = rep6.replace(/True/g, "sach");
+            let rep8 = rep7.replace(/False/g, "jhoot");
+            let rep9 = rep8.replace(/\(/g, " ");
+            let rep10 = rep9.replace(/\)/g, "");
+            let rep11 = rep10.replace(/:/g, "\n");
+            document.getElementById('modi-code').innerHTML = rep11;
+
+    }
